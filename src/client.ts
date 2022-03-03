2afa3bc986d5081b7865892451d7df949683a93d
RonaldColyar <phantexcreations@gmail.com>
2022-03-02 20:56:17 -0600

    breaks

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import WebSocket from "isomorphic-ws";
-import { BaseUser, GetFollowListResponse } from ".";
+import { BaseUser, GetFollowListResponse, UserPreview } from ".";
 import { 
     AuthResponse,AllUsersInRoomResponse, AuthCredentials, BasicRequest, 
     BasicResponse, CommunicationRoom, DeafAndMuteStatusUpdate, 
@@ -69,133 +69,168 @@ export class Client{
      */
      public route(e:WebSocket.MessageEvent){
         let basic_response:BasicResponse = JSON.parse(e.data.toString());
-        console.log(basic_response);
+        console.log("inside_routing:",basic_response.response_op_code);
+        console.log("inside_routing_data:",basic_response.response_containing_data);
         switch(basic_response.response_op_code){
             case "room_permissions":{
                 if (this.client_sub.all_room_permissions){
                     this.client_sub.all_room_permissions(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "user_mute_and_deaf_update":{
                 if (this.client_sub.deaf_and_mute_update){
                     this.client_sub.deaf_and_mute_update(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "@send-track-recv-done":{
                 if (this.client_sub.send_track_recv_done){
                     this.client_sub.send_track_recv_done(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "@send-track-send-done":{
                 if (this.client_sub.send_track_send_done){
                     this.client_sub.send_track_send_done(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "@connect-transport-recv-done":{
                 if (this.client_sub.connect_transport_recv_done){
                     this.client_sub.connect_transport_recv_done(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "@connect-transport-send-done":{
                 if (this.client_sub.connect_transport_send_done){
                     this.client_sub.connect_transport_send_done(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "invalid_request":{
                 if (this.client_sub.invalid_request){
                     this.client_sub.invalid_request(basic_response.response_containing_data);
                 }
+                break
             }
             case "user_hand_lowered":{
                 if (this.client_sub.user_hand_lowered){
                     this.client_sub.user_hand_lowered(basic_response.response_containing_data);
                 }
+                break
             }
             case "user_asking_to_speak":{
                 if (this.client_sub.user_hand_raised){
                     this.client_sub.user_hand_raised(basic_response.response_containing_data);
                 }
+                break
             }
             case "issue_creating_room"||"issue_blocking_user"||"issue_adding_speaker":{
                 if(this.client_sub.internal_error){
                     this.client_sub.internal_error(basic_response.response_containing_data);
                 }
+                break
             }
             case "room_meta_update":{
                 if (this.client_sub.room_update){
                     this.client_sub.room_update(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "auth-not-good":{
                 if (this.client_sub.bad_auth){
                     this.client_sub.bad_auth(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "auth-good":{
                 if (this.client_sub.good_auth){
                     this.client_sub.good_auth(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "top_rooms":{
                 if (this.client_sub.top_rooms){
                     this.client_sub.top_rooms(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "you-joined-as-speaker":{
                 if (this.client_sub.you_joined_as_speaker){
                     this.client_sub.you_joined_as_speaker(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "you-joined-as-peer":{
                 if(this.client_sub.you_joined_as_peer){
                     this.client_sub.you_joined_as_peer(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "you_left_room":{
                 if(this.client_sub.you_left_room){
                     this.client_sub.you_left_room(basic_response.response_containing_data);
                 }
+                break
             }
             case "new_user_joined":{
                 if(this.client_sub.new_user_joined){
                     this.client_sub.new_user_joined(basic_response.response_containing_data);
                 }
+                break
             }
             case "all_users_for_room":{
                 if(this.client_sub.all_users_in_room){
                     this.client_sub.all_users_in_room(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "speaker_removed":{
                 if(this.client_sub.speaker_removed){
                     this.client_sub.speaker_removed(basic_response.response_containing_data);
                 }
+                break
             }
             case "new_speaker":{
                 if(this.client_sub.new_speaker){
                     this.client_sub.new_speaker(basic_response.response_containing_data);
                 }
+                break
             }
             case "new-peer-speaker":{
                 if(this.client_sub.new_peer_speaker){
                     this.client_sub.new_peer_speaker(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "@get-recv-tracks-done":{
                 if(this.client_sub.get_recv_tracks_done){
                     this.client_sub.get_recv_tracks_done(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "your_data":{
                 if(this.client_sub.your_data){
                     this.client_sub.your_data(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
             case "follow_list_response":{
                 if(this.client_sub.followers){
                     this.client_sub.followers(JSON.parse(basic_response.response_containing_data));
                 }
+                break
             }
+            case "user_previews":{
+                if(this.client_sub.user_previews){
+                    this.client_sub.user_previews(JSON.parse(basic_response.response_containing_data));
+                }
+                break
+            }
+            default:
+                console.log('general error:',basic_response,basic_response.response_op_code == "top_rooms" || basic_response.response_containing_data == "your_data");
+                break
         }
      }
 }
@@ -282,6 +317,10 @@ export class ClientSubscriber{
      * When the server notifies you that a new user has joined your room
      */
     public new_user_joined:Nullable<Handler<StringifiedUserId>> = null; 
+    /**
+     * When the server sends you user previews
+     */
+    public user_previews:Nullable<Handler<Map<number,UserPreview>>> = null;
     /**
      * When the server notifies you that there is a new speaker
      */
